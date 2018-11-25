261a2aa3ac8bb437e7eae04d76400f977f43c82b
lrush85 <lance.rush1@gmail.com>
2018-11-25 00:40:05 -0600

    Added files created game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,193 @@
+window.onload = function() {
+
+    //Create all variables need for the game.
+
+var lives;
+var counter;
+var space;
+var guesses = [];
+var guess;
 var words = [
-    "cowboys",
-    "bears",
-    "rams",
-    "eagles",
-    "patriots",
-    "steelers",
-    "chiefs",
-    "saints",
-    "packers",
-    "redskins",
-    "vikings",
-    "seahawks",
-    "giants",
-    "lions",
-    "raiders",
-    "falcons",
-    "broncos",
-    "browns",
-    "ravens",
-    "panthers",
-    "texans",
-    "fortyniners",
-    "cardinals",
-    "chargers",
-    "colts",
-    "dolphins",
-    "jaguars",
-    "bengles",
-    "bills",
-    "jets",
-    "titans",
-    "buccaneers",
-    "bills"
+    "COWBOYS",
+    "BEARS",
+    "RAMS",
+    "EAGLES",
+    "PATRIOTS",
+    "STEELERS",
+    "CHIEFS",
+    "SAINTS",
+    "PACKERS",
+    "REDSKINS",
+    "VIKINGS",
+    "SEAHAWKS",
+    "GIANTS",
+    "LIONS",
+    "RAIDERS",
+    "FALCONS",
+    "BRONCOS",
+    "BROWNS",
+    "RAVENS",
+    "PANTHERS",
+    "TEXANS",
+    "FOURTY NINERS",
+    "CARDINALS",
+    "CHARGERS",
+    "COLTS",
+    "DOLPHINS",
+    "JAGUARS",
+    "BENGALS",
+    "BILLS",
+    "JETS",
+    "TITANS",
+    "BUCCANEERS"
 ];
-// Pick Random Word
+
 var word = words[Math.floor(Math.random() * words.length)];
-
-// Answer setup
-
-var userName = prompt("What is your name?");
-document.getElementById("userName").innerHTML = userName + ", WELCOME!!! This is the NFL Guess Game... lets get started.  Choose a letter and try to figure out what team we are displaying!!! GOOD LUCK!";
-
 var alpha = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
 ];
 
-var ul = document.createElement("ul");
-document.getElementById("letters").appendChild(ul);
+var livesRemaining = document.getElementById("lives");
 
-alpha.forEach(function(letter) {
-    var li = document.createElement("li");
-    ul.appendChild(li);
-    var button = document.createElement("button");
-    li.appendChild(button);
-    button.innerHTML += letter;
-});
+// Create letters list of alphabet function
+// I created a buttons function that hold the alphabet with styles
+function buttons() {
 
-// display random word "_" below buttons
-var answer = [];
-for (var w = 0; w < word.length; w++) {
-    answer[w] = "_";
+    alphaButtons = document.getElementById('buttons');
+    letters = document.createElement("ul");
+
+    for(var i = 0; i < alpha.length; i++) {
+        letters.id = "alpha";
+        list = document.createElement("li");
+        list.id = "letter";
+        list.innerHTML = alpha[i];
+        clicked();
+        alphaButtons.appendChild(letters);
+        letters.appendChild(list);
+    }
 }
 
-var remainingLetters = word.length;
+// Create a function to display result of correct or incorrect guess within the random word selected
 
-// If alpha selected, keep active and then push selected alpha or (letter in array) to answer array and join to the random word 
+function result() {
+    wordHolder = document.getElementById("word");
+    correct = document.createElement("ul");
 
-var displayAnswer = document.getElementById("answer").append(answer.join(" "));
+    for(var i = 0; i < word.length; i++) {
+        correct.setAttribute("id", "correct");
+        guess = document.createElement("li");
+        guess.setAttribute("class", "guess");
+        // if word index is equal to "-" then add guess to "-"
+        // and space = 1
+        // else add guess to " _ "
 
-document.getElementById("hold").addEventListener("click", function (e) {
-    
-});
+        if (word[i] === "-") {
+            guess.innerHTML = "-";
+            space = 1;
+        } else {
+            guess.innerHTML = " _ ";
+        }
 
+        // Push result to guesses array
+        // Append to ul and li
+
+        guesses.push(guess);
+        wordHolder.appendChild(correct);
+        correct.appendChild(guess);
+    }
+}
+
+// Create a function that displays how many lives left, if you win or lose
+
+function status() {
+    livesRemaining.innerHTML = "You have " + lives + " lives remaining.";
+
+    // If lives is less than 1 display game over, and an alert
+    if (lives < 1) {
+        livesRemaining.innerHTML = "Game Over!";
+    }
+    // Loop through guesses
+    for (var i = 0; i < guesses.length; i++) {
+        if (counter + space === guesses.length) {
+            livesRemaining.innerHTML = "You Win!";
+        }
+    }
+}
+
+// Create a function that reacts to click of the li, handles change of status function and added guess to result if correct
+
+function clicked() {
+    list.onclick = function () {
+        var guess = this.innerHTML;
+        this.setAttribute("class", "active");
+        this.onclick = null;
+
+        // Loop through guesses and if guess is equal to letter in word display result, then increment the count
+        for(var i = 0; i < word.length; i++) {
+            if (word[i] === guess) {
+                guesses[i].innerHTML = guess;
+                counter += 1;
+            }
+        }
+        //If the index of the guess is -1 then we want to decrement the lives remaining
+        var indexWord = word.indexOf(guess);
+        if (indexWord === -1) {
+            lives -= 1;
+            status();
+        } else {
+            status();
+        }
+    };
+}
+
+// Create the function that allows user to play the game
+function play() {
+    word = words[Math.floor(Math.random() * words.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    guesses = [];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    status();
+}
+
+play();
+
+
+// Reset the game
+    document.getElementById("reset").onclick = function () {
+        correct.parentNode.removeChild(correct);
+        letters.parentNode.removeChild(letters);
+
+    play();
+    };
+};
